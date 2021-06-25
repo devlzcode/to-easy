5d21ccb6dddc098249be5f4e99729f8d9a36b0e2
lenneth <developer.lenneth@gmail.com>
2021-06-25 13:57:25 -0700

    Initial

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+type ToResult<T, U = Error> = [undefined, T] | [U, undefined]
+
+/**
+ * @param { Promise } asyncFn The asynchronous function
+ */
+export const asyncTo = <T, U = Error>(
+    asyncFn: Promise<T>
+): Promise<ToResult<T, U>> => asyncFn
+    .then<[undefined, T]>((result: T) => [undefined, result])
+    .catch<[U, undefined]>((e: U) => [e, undefined])
+
+/**
+ * @param { Function } syncFn The synchronous function
+ */
+export const syncTo = <T, U = Error>(
+    syncFn: (...rest: any[]) => T,
+    ...rest: any[]
+): ToResult<T, U> => {
+    try {
+        const result = syncFn(...rest)
+        return [undefined, result]
+    } catch(e) {
+        return [<U>e, undefined]
+    }
+}
